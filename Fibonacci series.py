7c0b50850b912fdbc90029914f69db5ba9a9e058
Rosaline Roshni Jeevananth <107970776+Roshni20001102@users.noreply.github.com>
2024-07-21 19:31:21 +0530

    Add files via upload

---
+++ Fibonacci series.py
@@ -0,0 +1,13 @@
+nterms = int(input("Enter the number of terms in Fibnoacci Series: "))
+
+n1,n2 = 0,1
+n3 = 0
+
+
+print("Fibnoacci series: {}, {}, ".format(n1, n2), end=" ")
+for i in range (0,nterms - 2):
+    n3 = n1 + n2
+    print("{}, ".format(n3), end=" ")
+    n1 = n2
+    n2 = n3
+
